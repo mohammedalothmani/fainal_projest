b694c361d5fdab463343db5a4862fefdeb354e66
mohammedalothmani <mohammedalothmani2121@gmail.com>
2023-06-20 17:06:22 +0300

    todo 1 class course

--- finall.py
+++ finall.py
@@ -1,2 +1,13 @@
+import uuid
 # Name : mohammed alothmani
 # Delivery Date :20/06/2023
+# TODO 2 Define Course Class and define constructor with
+# course_id (generated using uuid4) ,
+# course name (user_input) and
+# course mark (user_input)
+class Course :
+    def __init__(self, course_name, course_mark):
+        self.course_id = uuid.uuid4()
+        self.course_name = course_name
+        self.course_mark = course_mark
+
